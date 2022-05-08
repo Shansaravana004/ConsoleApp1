7d50cb7f7dfdc9dd2ecdc1e6cc68b69ef71da31d
Saravana <Saravana@DESKTOP-C0IBRGI>
2022-05-08 12:11:15 +0530

    Add project files.

---
+++ ConsoleApp1/Task.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1
+{
+	internal class Task
+	{
+		static void Main(string[] args)
+		{
+			int inputLength = int.Parse(Console.ReadLine() ?? "");
+			string arrayElementStr = Console.ReadLine() ?? "";
+			string[] arrayString = arrayElementStr.Split(' ');
+			string strresult = string.Empty;
+			for (int i = 0; i < arrayString.Length; i++)
+			{
+				strresult += arrayString[i][arrayString[i].Length - 1].ToString();
+			}
+
+			if(int.Parse(strresult) % 10 == 0)
+			{
+				Console.WriteLine("Yes");
+			}
+			else
+			{
+				Console.WriteLine("No");
+			}
+			
+		}
+	}
+}
